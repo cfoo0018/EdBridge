0c30294e15dc38c45ff8b6389a04f1753865f192
cfoo0018 <cfoo001@student.monnash.edu>
2024-03-29 12:21:01 +1100

    tailwind setup

--- storage/framework/views/d792e356ab06f404a6aefec9e5cb0f9fe74a71d3.php
+++ storage/framework/views/d792e356ab06f404a6aefec9e5cb0f9fe74a71d3.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
         <title>Laravel</title>
 
         <!-- Fonts -->
