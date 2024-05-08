3093f101e437317112fdd1dad28d6db3fb50b749
Alex <alya0001@student.monash.edu>
2024-05-08 14:57:26 +1000

    stop incurring money 500 my api key was leaked

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('geocode:charities')->daily();
+        // $schedule->command('geocode:charities')->daily();
     }
 
     /**
