f4207da2195f8fdab65caf2a6fbbe4051f60204f
Alex <alya0001@student.monash.edu>
2024-05-13 03:05:22 +1000

    Navbar and Routing Grouped

--- routes/web.php
+++ routes/web.php
@@ -31,42 +31,59 @@ Route::prefix('support')->group(function() {
 Route::get('/resourcehub', [YouTubeController::class, 'index'])->name('resourcehub');
 Route::get('/resourcehub/search', [YouTubeController::class, 'search'])->name('youtube.search');
 
-Route::get('/pathways', function () {
-    return view('pathways.pathways');
-})->name('pathways');
+// Route::get('/pathways', function () {
+//     return view('pathways.pathways');
+// })->name('pathways');
 
-Route::get('/datascience', function () {
-    return view('pathways.roadmaps.datascience');
-})->name('datascience');
+// Route::get('/datascience', function () {
+//     return view('pathways.roadmaps.datascience');
+// })->name('datascience');
 
-Route::get('/datascience/quiz', function () {
-    return view('pathways.quiz.datascience');
-})->name('datasciencequiz');
+// Route::get('/datascience/quiz', function () {
+//     return view('pathways.quiz.datascience');
+// })->name('datasciencequiz');
 
-Route::get('/softwaredevelopment', function () {
-    return view('pathways.roadmaps.softwaredevelopment');
-})->name('softwaredevelopment');
+// Route::get('/softwaredevelopment', function () {
+//     return view('pathways.roadmaps.softwaredevelopment');
+// })->name('softwaredevelopment');
 
-Route::get('/softwaredevelopment/quiz', function () {
-    return view('pathways.quiz.softwaredevelopment');
-})->name('softwaredevelopmentquiz');
+// Route::get('/softwaredevelopment/quiz', function () {
+//     return view('pathways.quiz.softwaredevelopment');
+// })->name('softwaredevelopmentquiz');
 
-Route::get('/cybersecurity', function () {
-    return view('pathways.roadmaps.cybersecurity');
-})->name('cybersecurity');
+// Route::get('/cybersecurity', function () {
+//     return view('pathways.roadmaps.cybersecurity');
+// })->name('cybersecurity');
 
-Route::get('/cybersecurity/quiz', function () {
-    return view('pathways.quiz.cybersecurity');
-})->name('cybersecurityquiz');
+// Route::get('/cybersecurity/quiz', function () {
+//     return view('pathways.quiz.cybersecurity');
+// })->name('cybersecurityquiz');
 
-Route::get('/businessinformation', function () {
-    return view('pathways.roadmaps.businessinformation');
-})->name('businessinformation');
+// Route::get('/businessinformation', function () {
+//     return view('pathways.roadmaps.businessinformation');
+// })->name('businessinformation');
 
-Route::get('/businessinformation/quiz', function () {
-    return view('pathways.quiz.businessinformation');
-})->name('businessinformationquiz');
+// Route::get('/businessinformation/quiz', function () {
+//     return view('pathways.quiz.businessinformation');
+// })->name('businessinformationquiz');
 
+Route::prefix('pathways')->group(function () {
+    Route::view('/', 'pathways.pathways')->name('pathways');
+
+    Route::prefix('roadmaps')->group(function () {
+        Route::view('/datascience', 'pathways.roadmaps.datascience')->name('datascience');
+        Route::view('/softwaredevelopment', 'pathways.roadmaps.softwaredevelopment')->name('softwaredevelopment');
+        Route::view('/cybersecurity', 'pathways.roadmaps.cybersecurity')->name('cybersecurity');
+        Route::view('/businessinformation', 'pathways.roadmaps.businessinformation')->name('businessinformation');
+    });
+
+    Route::prefix('quiz')->group(function () {
+        Route::view('/datascience', 'pathways.quiz.datascience')->name('datasciencequiz');
+        Route::view('/softwaredevelopment', 'pathways.quiz.softwaredevelopment')->name('softwaredevelopmentquiz');
+        Route::view('/cybersecurity', 'pathways.quiz.cybersecurity')->name('cybersecurityquiz');
+        Route::view('/businessinformation', 'pathways.quiz.businessinformation')->name('businessinformationquiz');
+    });
+});
 
 Route::get('/scholarships', [ScholarshipController::class, 'index'])->name('scholarships.index');
 Route::get('/scholarships/{id}', [ScholarshipController::class,'show'])->name('scholarships.show');
